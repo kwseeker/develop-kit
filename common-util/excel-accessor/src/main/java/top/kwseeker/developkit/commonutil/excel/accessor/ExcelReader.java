c2638974960be3a702c9e8ca11fae89ca6faf8c4
kwseekerli <kwseekerli@welove-inc.com>
2021-03-19 12:00:02 +0800

    添加计划任务

---
+++ common-util/excel-accessor/src/main/java/top/kwseeker/developkit/commonutil/excel/accessor/ExcelReader.java
@@ -0,0 +1,4 @@
+package top.kwseeker.developkit.commonutil.excel.accessor;
+
+public class ExcelReader {
+}
---
+++ common-util/excel-accessor/src/main/java/top/kwseeker/developkit/commonutil/excel/accessor/ExcelWriter.java
@@ -0,0 +1,9 @@
+package top.kwseeker.developkit.commonutil.excel.accessor;
+
+/**
+ * 将集合(List<Object> Map<String, Object> 按照成员名分列写入.xlsx文档)
+ * 通过注解控制哪些字段写入以及列的顺序
+ */
+public class ExcelWriter {
+
+}
